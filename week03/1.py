6e5cbb2ee3be7944569c6323dfe4403ddd7cc4cd
RootUnvalue <mls1218@naver.com>
2026-03-17 08:28:24 +0900

    update

--- week03/1.py
+++ week03/1.py
@@ -1,45 +1,45 @@
 import math
-# print("Hello, World!"); print("after semicolon")
+print("Hello, World!"); print("after semicolon")
 
-# print('ABC'); print('ABC')
+print('ABC'); print('ABC')
 
-# print("%d" % 123)
-# print("%d %d" % (123, 456))
-# print("%f" % 3.1415926)
-# print("%c" % 65)
-# print("%c" % 'A')
-# print("%s" % "string")
-
-
-# print("%5d" % 1234)
-# print("%05d" % 1234)
-
-# print("%f" % 3.141592653589793)
-# print("%1.10f" % 3.141592653589793)
-# print("%7.2f" % 3.141592653589793)
-
-# print("%s" % "Python")
-# print("%10s" % "Python")
+print("%d" % 123)
+print("%d %d" % (123, 456))
+print("%f" % 3.1415926)
+print("%c" % 65)
+print("%c" % 'A')
+print("%s" % "string")
 
 
-# print("{0:d}, {1:5d}, {2:05d}" .format(123, 123, 123))
-# print("{2:d}, {1:d}, {0:d}" .format(100, 200, 300))
+print("%5d" % 1234)
+print("%05d" % 1234)
 
-# print("줄 바꾸기 \n 줄바뀜")
-# print("탭 하기 \t하지만 빨랐쥬")
-# print("\"따옴표\"")
-# print("\'작은 따옴표\'")
-# print("\\백슬래시\\")
-# print("123\r캐리지 리턴. 후 백슬래시\b")
-# print(r"\n \t \" \\가 그대로 나옵니다.")
+print("%f" % 3.141592653589793)
+print("%1.10f" % 3.141592653589793)
+print("%7.2f" % 3.141592653589793)
 
-# print(1, 2, 3)
-# print("안녕", "하시기")
-# print("안녕", "하시기", sep="?")
-# print(1920, 1080, sep="x")
-# print("안녕", end="")
-# print("하시기")
+print("%s" % "Python")
+print("%10s" % "Python")
 
-# a = 1; b = 2; c = 3
-# print(a, b, c, sep=" 그 뒤에는 ")
-# del a, b, c
+
+print("{0:d}, {1:5d}, {2:05d}" .format(123, 123, 123))
+print("{2:d}, {1:d}, {0:d}" .format(100, 200, 300))
+
+print("줄 바꾸기 \n 줄바뀜")
+print("탭 하기 \t하지만 빨랐쥬")
+print("\"따옴표\"")
+print("\'작은 따옴표\'")
+print("\\백슬래시\\")
+print("123\r캐리지 리턴. 후 백슬래시\b")
+print(r"\n \t \" \\가 그대로 나옵니다.")
+
+print(1, 2, 3)
+print("안녕", "하시기")
+print("안녕", "하시기", sep="?")
+print(1920, 1080, sep="x")
+print("안녕", end="")
+print("하시기")
+
+a = 1; b = 2; c = 3
+print(a, b, c, sep=" 그 뒤에는 ")
+del a, b, c
