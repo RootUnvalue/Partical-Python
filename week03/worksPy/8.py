2025dc413109bbd1a16d7210a84d8078a95c0520
RootUnvalue <mls1218@naver.com>
2026-03-21 17:36:26 +0900

    update

--- week03/worksPy/8.py
+++ week03/worksPy/8.py
@@ -1,3 +1,11 @@
 print("%d" % 42)
 print("%5d" % 42)
 print("%07d" % 42)
+
+print("%d" % 1235)
+print("%5d" % 1235)
+print("%07d" % 1235)
+
+print("%d" % 21346)
+print("%5d" % 21346)
+print("%07d" % 21346)
