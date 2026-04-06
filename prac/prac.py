c1aa3d5c0fbff045dce90de7dbfd7fb4ae77657a
RootUnvalue <mls1218@naver.com>
2026-04-06 16:24:37 +0900

    update

--- prac/prac.py
+++ prac/prac.py
@@ -1,12 +1,16 @@
-import turtle
+#16
+a = int(input("a 값을 20아래의 수를 압력하세요"))
+b = int(input("b 값을 20아래의 수를 압력하세요"))
+ 
+if a < 20 and b < 20:
+    print("a와 b 둘다 20보다 작은 값이군요")
+    if a < b:
+        print("b가 더 큰값이군요")
+    else:
+        print("a가 더 큰값이군요")
 
-t = turtle.Turtle()
-t.speed(0)
-t.penup()
-t.goto(-200, 0)
-t.pendown()
-for i in range(36):
-    t.forward(100)
-    t.backward(100)
-    t.left(10)
-turtle.done()
+elif a > 20 and b > 20:
+    print("a와b 모두 20보다 큰 값이군요")
+
+else:
+    print("a와b 둘중 하나는 20보다 큰 값이군요")
