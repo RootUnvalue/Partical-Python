0e057808dee166fa8c2323bd848f82db371bc12e
RootUnvalue <mls1218@naver.com>
2026-04-14 00:58:20 +0900

    update

--- prac/prac.py
+++ prac/prac.py
@@ -1,4 +1,13 @@
-#13
-for i in range(1, 101):
-    if i % 6 is not 0:
-        print(i, end=", ")
+#12
+mod3 = ""
+mod7 = ""
+
+for i in range(1, 21):
+    if i % 3 == 0:
+        mod3 += str(i) + ", "
+    elif i % 7 == 0:
+        mod7 += str(i) + ", "
+
+print(f"1 부터 20까지의 수중\n3의 배수인 : {mod3}\n7의 배수인 수: {mod7}")
+        
+        
