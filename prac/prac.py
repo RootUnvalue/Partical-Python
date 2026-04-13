f3b5c7ca71fbe42c598b7de0704071af42ba2c16
RootUnvalue <mls1218@naver.com>
2026-04-13 15:33:59 +0900

    update

--- prac/prac.py
+++ prac/prac.py
@@ -1,16 +1,4 @@
-#16
-a = int(input("a 값을 20아래의 수를 압력하세요"))
-b = int(input("b 값을 20아래의 수를 압력하세요"))
- 
-if a < 20 and b < 20:
-    print("a와 b 둘다 20보다 작은 값이군요")
-    if a < b:
-        print("b가 더 큰값이군요")
-    else:
-        print("a가 더 큰값이군요")
-
-elif a > 20 and b > 20:
-    print("a와b 모두 20보다 큰 값이군요")
-
-else:
-    print("a와b 둘중 하나는 20보다 큰 값이군요")
+#13
+for i in range(1, 101):
+    if i % 6 is not 0:
+        print(i, end=", ")
